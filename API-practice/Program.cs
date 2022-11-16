f330d3915d945e3243c33b48f26852a111e143c3
Albin Brolin <crystallizedtester@gmail.com>
2022-11-16 12:45:18 +0100

    yes

---
+++ API-practice/Program.cs
@@ -0,0 +1,22 @@
+﻿using RestSharp;
+using System.Text.Json;
+using System.Net;
+
+RestClient pokeClient = new("https://pokeapi.co/api/v2/");
+RestRequest request = new("pokemon/amoonguss");
+RestResponse response = pokeClient.GetAsync(request).Result;
+
+if (response.StatusCode == HttpStatusCode.OK)
+{
+    Pokemon p = JsonSerializer.Deserialize<Pokemon>(response.Content);
+    Console.WriteLine(p.Name);
+    Console.WriteLine(p.Weight);
+}
+else
+{
+    Console.WriteLine("What?");
+}
+
+// Console.WriteLine(response.Content);
+
+Console.ReadLine();
